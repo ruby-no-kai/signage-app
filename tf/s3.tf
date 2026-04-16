809255a1c179c54b09337f5c9a08f5b2a8e09efa
Daisuke Aritomo <daisuke.aritomo@smarthr.co.jp>
2026-04-16 17:26:36 +0900

    public bucket: Allow CORS access

--- tf/s3.tf
+++ tf/s3.tf
@@ -34,6 +34,16 @@ resource "aws_s3_bucket_policy" "public" {
   depends_on = [aws_s3_bucket_public_access_block.public]
 }
 
+resource "aws_s3_bucket_cors_configuration" "public" {
+  bucket = aws_s3_bucket.public.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+  }
+}
+
 resource "aws_s3_object" "config" {
   count         = var.manage_config_in_s3 ? 1 : 0
   bucket        = aws_s3_bucket.public.bucket
